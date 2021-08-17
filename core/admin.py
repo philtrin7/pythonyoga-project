da3c3fa0304ea4a28db6cb48aade49724a393081
philtrin7 <philtrin7@gmail.com>
2021-08-17 12:01:55 +1000

    added new Category and Job models

--- core/admin.py
+++ core/admin.py
@@ -3,3 +3,5 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Customer)
+admin.site.register(models.Category)
+admin.site.register(models.Job)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import uuid
+from django.utils import timezone
+
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,3 +16,53 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+
+
+class Category(models.Model):
+    slug = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    SMALL_SIZE = "small"
+    MEDIUM_SIZE = "medium"
+    LARGE_SIZE = "large"
+    SIZES = (
+        (SMALL_SIZE, 'Small'),
+        (MEDIUM_SIZE, 'Medium'),
+        (LARGE_SIZE, 'Large')
+    )
+
+    CREATING_STATUS = 'creating'
+    PROCESSING_STATUS = 'processing'
+    PICKING_STATUS = 'picking'
+    DELIVERING_STATUS = 'delivering'
+    COMPLETED_STATUS = 'completed'
+    CANCELLED_STATUS = 'cancelled'
+    STATUSES = (
+        (CREATING_STATUS, 'Creating'),
+        (PROCESSING_STATUS, 'Processing'),
+        (PICKING_STATUS, 'Picking'),
+        (DELIVERING_STATUS, 'Delivering'),
+        (COMPLETED_STATUS, 'Completed'),
+        (CANCELLED_STATUS, 'Cancelled'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    size = models.CharField(max_length=20, choices=SIZES, default=MEDIUM_SIZE)
+    quantity = models.IntegerField(default=1)
+    photo = models.ImageField(upload_to="job/photos/")
+    status = models.CharField(
+        max_length=20, choices=STATUSES, default=CREATING_STATUS)
+    create_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
