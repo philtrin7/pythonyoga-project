57c67bf0b44f1886b0341d6444fd562b5df7059a
philtrin7 <philtrin7@gmail.com>
2021-08-17 17:33:41 +1000

    hooked step2_form post and field population

--- core/customer/views.py
+++ core/customer/views.py
@@ -143,6 +143,13 @@ def create_job_page(request):
                 creating_job.save()
                 return redirect(reverse('customer:create_job'))
 
+        elif request.POST.get('step') == '2':
+            step2_form = forms.JobCreateStep2Form(
+                request.POST, instance=creating_job)
+            if step2_form.is_valid():
+                creating_job = step2_form.save()
+                return redirect(reverse('customer:create_job'))
+
     # Determine the current step
     if not creating_job:
         current_step = 1
