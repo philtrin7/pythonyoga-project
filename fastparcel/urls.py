8d00596bfd295d19893b6d9b4247914b95812be8
philtrin7 <philtrin7@gmail.com>
2021-08-22 14:44:22 +1000

    hooked up views/urls to Courier's available_jobs page

--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 from core import views
 
 from core.customer import views as customer_views
-from core.customer import views as courier_views
+from core.courier import views as courier_views
 
 customer_urlpatterns = [
     path('', customer_views.home, name='home'),
@@ -21,7 +21,8 @@ customer_urlpatterns = [
 ]
 
 courier_urlpatterns = [
-    path('', courier_views.home, name='home')
+    path('', courier_views.home, name='home'),
+    path('jobs/available/', courier_views.available_jobs_page, name="available_jobs")
 ]
 
 urlpatterns = [
