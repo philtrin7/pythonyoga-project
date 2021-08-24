69dde3c7575f7a9909d673116ee90bc3a6b5ee74
philtrin7 <philtrin7@gmail.com>
2021-08-24 22:07:30 +1000

    connected channels-redis and enabled channel layer

--- core/consumers.py
+++ core/consumers.py
@@ -1,14 +1,29 @@
 import json
+from asgiref.sync import async_to_sync
+
 from channels.generic.websocket import WebsocketConsumer
 from . import models
 
 
 class JobConsumer(WebsocketConsumer):
     def connect(self):
+        self.job_id = self.scope['url_route']['kwargs']['job_id']
+        self.job_group_name = 'job_%s' % self.job_id
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.job_group_name,
+            self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.job_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -18,7 +33,19 @@ class JobConsumer(WebsocketConsumer):
             self.scope['user'].courier.lat = job['courier_lat']
             self.scope['user'].courier.lng = job['courier_lng']
             self.scope['user'].courier.save()
-        # print("Job", job)
-        # self.send(text_data=json.dumps({
-        #     'message': message
-        # }))
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.job_group_name,
+            {
+                'type': 'job_update',
+                'job': job
+            }
+        )
+
+    def job_update(self, event):
+        job = event['job']
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'job': job
+        }))
